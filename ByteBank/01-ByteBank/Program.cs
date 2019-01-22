0dacf1bee9bf615052e94c64f14d674421be999e
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-01-21 22:34:22 -0200

    A2 usando nossa classe

--- ByteBank/01-ByteBank/Program.cs
+++ ByteBank/01-ByteBank/Program.cs
@@ -10,7 +10,20 @@ namespace _01_ByteBank
     {
         static void Main(string[] args)
         {
-            new ContaCorrente();
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+            contaDaGabriela.saldo = 100;
+
+            Console.WriteLine(contaDaGabriela.titular);
+            Console.WriteLine("Agência: " + contaDaGabriela.agencia);
+            Console.WriteLine("Número: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+
+            contaDaGabriela.saldo += 200;
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
 
             Console.ReadLine();
         }
