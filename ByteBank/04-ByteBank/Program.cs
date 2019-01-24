377c12854a8f898e7e983679edf1904098fae07e
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-01-24 21:56:10 -0200

    A3 metodo com varios argumentos

--- ByteBank/04-ByteBank/Program.cs
+++ ByteBank/04-ByteBank/Program.cs
@@ -16,15 +16,34 @@ namespace _04_ByteBank
 
             Console.WriteLine(contaDoBruno.saldo);
 
-            bool resultado = contaDoBruno.Sacar(50);
-
-            Console.WriteLine(contaDoBruno.saldo);
+            bool resultado = contaDoBruno.Sacar(500);
 
             Console.WriteLine(resultado);
+
+            Console.WriteLine(contaDoBruno.saldo);
 
             contaDoBruno.Depositar(500);
 
             Console.WriteLine(contaDoBruno.saldo);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo da Gabriela: " + contaDaGabriela.saldo);
+
+            bool resultadoTransferencia = contaDoBruno.Transferir(200, contaDaGabriela);
+
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo da Gabriela: " + contaDaGabriela.saldo);
+
+            Console.WriteLine(resultadoTransferencia);
+
+            contaDaGabriela.Transferir(100, contaDoBruno);
+
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo da Gabriela: " + contaDaGabriela.saldo);
 
             Console.ReadLine();
         }
