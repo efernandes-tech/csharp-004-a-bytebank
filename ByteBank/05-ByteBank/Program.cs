f57bc9e2dae158fb6c47bdd0457ca81ff0b17ecb
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-01-24 22:31:22 -0200

    A4 referencias nulas

--- ByteBank/05-ByteBank/Program.cs
+++ ByteBank/05-ByteBank/Program.cs
@@ -10,26 +10,39 @@ namespace _05_ByteBank
     {
         static void Main(string[] args)
         {
-            Cliente gabriela = new Cliente();
+            // Cliente gabriela = new Cliente();
 
-            gabriela.nome = "Gabriela";
-            gabriela.profissao = "Desenvolvedora C#";
-            gabriela.cpf = "123.456.789-00";
+            // gabriela.nome = "Gabriela";
+            // gabriela.profissao = "Desenvolvedora C#";
+            // gabriela.cpf = "123.456.789-00";
 
             ContaCorrente conta = new ContaCorrente();
+            // conta.titular = gabriela;
 
-            conta.titular = gabriela;
+            // conta.titular = new Cliente();
+            // conta.titular.nome = "Gabriela";
+            // conta.titular.profissao = "Desenvolvedora C#";
+            // conta.titular.cpf = "123.456.789-00";
+            
             conta.saldo = 500;
             conta.agencia = 563;
             conta.numero = 12345;
 
-            Console.WriteLine(gabriela.nome);
-            Console.WriteLine(conta.titular.nome);
+            if (conta.titular == null)
+            {
+                Console.WriteLine("Ops, a referência em conta.titular é NULL");
+            }
 
-            conta.titular.nome = "Gabriela Costa";
+            // Console.WriteLine(gabriela.nome);
+            // Console.WriteLine(conta.titular.nome);
 
-            Console.WriteLine(gabriela.nome);
-            Console.WriteLine(conta.titular.nome);
+            // conta.titular.nome = "Gabriela Costa";
+
+            // Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular);
+            // Console.WriteLine(conta.titular.nome);
+            // Console.WriteLine(conta.titular.cpf);
+            // Console.WriteLine(conta.titular.profissao);
 
             Console.ReadLine();
         }
