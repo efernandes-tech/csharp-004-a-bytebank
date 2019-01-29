af94eff8a45f8827fd6fcadd1d1f9eedaca7a239
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-01-29 20:55:07 -0200

    A5 propriedades get e set

--- ByteBank/06-ByteBank/ContaCorrente.cs
+++ ByteBank/06-ByteBank/ContaCorrente.cs
@@ -8,51 +8,54 @@ namespace _06_ByteBank
 {
     public class ContaCorrente
     {
-        public Cliente titular;
+        public Cliente Titular { get; set; }
+
         public int agencia;
         public int numero;
-        private double saldo = 100;
+        private double _saldo = 100;
 
-        public void DefinirSaldo(double saldo)
+        public double Saldo
         {
-            if (saldo < 0)
+            get
             {
-                return;
+                return this._saldo;
             }
-            this.saldo = saldo;
-        }
-
-        public double ObeterSaldo()
-        {
-            return this.saldo;
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                this._saldo = value;
+            }
         }
 
         public bool Sacar(double valor)
         {
-            if (this.saldo < valor)
+            if (this._saldo < valor)
             {
                 return false;
             }
             else
             {
-                this.saldo -= valor;
+                this._saldo -= valor;
                 return true;
             }
         }
 
         public void Depositar(double valor)
         {
-            this.saldo += valor;
+            this._saldo += valor;
         }
 
         public bool Transferir(double valor, ContaCorrente contaDestino)
         {
-            if (this.saldo < valor)
+            if (this._saldo < valor)
             {
                 return false;
             }
 
-            this.saldo -= valor;
+            this._saldo -= valor;
             contaDestino.Depositar(valor);
             return true;
         }
