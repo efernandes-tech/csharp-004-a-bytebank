ecb2c9c932d54df5412727ca09ad2cc49980886c
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-01-29 21:25:32 -0200

    A6 construtores

---
+++ ByteBank/07-ByteBank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(867, 86712540);
+
+            // conta.Agencia = 867;
+            conta.Numero = 86712540;            
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
+        }
+    }
+}
